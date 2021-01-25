862b8ec542230e664d9e644b25db82e18cd8c8e0
dubis78 <dubis85201@hotmail.com>
2021-01-24 20:14:07 -0500

    CRUD working

---
+++ SQL/db.sql
@@ -0,0 +1,18 @@
+  
+CREATE DATABASE firstapi;
+
+--\l
+
+--\c firstapi;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    email TEXT
+);
+
+INSERT INTO users (name, email)
+    VALUES ('joe', 'joe@ibm.com'),
+    ('ryan', 'ryan@faztweb.com');
+
+select * from users;
--- src/consults/consults.js
+++ src/consults/consults.js
@@ -1,45 +1,60 @@
 const pool= require('../config/database');
 
+const vetData=(rBody,rParams,rQuery)=>{
+    const data=rBody.concat(rParams.concat(rQuery));
+    if(data.length>0){
+      return data;
+    }
+    else{
+      return [];
+    }
+  }
+
+const consult=async(sql,req)=>{
+    console.log(req.body,req.params,req.query);
+    try {
+      const rBody=Object.values(req.body),rParams=Object.values(req.params),rQuery=Object.values(req.query);
+      const data=vetData(rBody,rParams,rQuery);
+      console.log(data);
+      const {rows}=await pool.query(sql,data);
+      return rows;
+    } catch (e) {
+      return {errorCode : e.errno, message : "Error en el servidor"};
+    }
+  }
+  
+
 const getUsers = async (req, res) => {
-    const response = await pool.query('SELECT * FROM users ORDER BY id ASC');
-    res.status(200).json(response.rows);
+    res.status(200).json(await consult('SELECT * FROM users ORDER BY id ASC',req));
 };
 
 const getUserById = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const response = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-    res.json(response.rows);
+    res.status(200).json(await consult('SELECT * FROM users WHERE id = $1',req));
 };
 
 const createUser = async (req, res) => {
+    const result =await consult('INSERT INTO users (name, email) VALUES ($1, $2)',req);
     const { name, email } = req.body;
-    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
-    res.json({
-        message: 'User Added successfully',
-        body: {
-            user: {name, email}
-        }
-    })
+    result.length==0 ?(
+    res.json({message: 'User Added successfully', body: {user: {name, email}}})
+    )
+    :(res.send(result));
 };
 
 const updateUser = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const { name, email } = req.body;
-
-    const response =await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [
-        name,
-        email,
-        id
-    ]);
-    res.json('User Updated Successfully');
+    const result =await consult('UPDATE users SET name = $1, email = $2 WHERE id = $3',req);
+    result.length==0 ?(
+        res.json('User Updated Successfully')
+        )
+        :(res.send(result));
 };
 
 const deleteUser = async (req, res) => {
-    const id = parseInt(req.params.id);
-    await pool.query('DELETE FROM users where id = $1', [
-        id
-    ]);
-    res.json(`User ${id} deleted Successfully`);
+    const result =await consult('DELETE FROM users where id = $1',req);
+    result.length==0 ?(
+        res.json(`User ${req.params.id} deleted Successfully`)
+        )
+        :(res.send(result));
 };
 
 module.exports = {getUsers, getUserById, createUser, updateUser, deleteUser};
